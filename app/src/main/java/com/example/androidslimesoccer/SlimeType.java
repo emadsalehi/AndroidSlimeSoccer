ae567e9300bfaa8a24a6e97b7939ae92a122e79d
Emad Salehi <emad.s1178@yahoo.com>
2019-06-26 22:11:54 +0430

    SlimeType Methods Added. Utils Updated.

--- app/src/main/java/com/example/androidslimesoccer/SlimeType.java
+++ app/src/main/java/com/example/androidslimesoccer/SlimeType.java
@@ -1,8 +1,30 @@
 package com.example.androidslimesoccer;
 
 public enum SlimeType {
-    CLASSIC,
-    INDIAN,
-    ALIEN,
-    TRAFIC
+    CLASSIC(300, 20, true),
+    INDIAN(400, 400, false),
+    ALIEN(600, 500, true),
+    TRAFIC(400, 200, true);
+
+    private final int specialThreshold;
+    private final int enableDecrease;
+    private final boolean isImmediate;
+
+    SlimeType(int specialThreshold, int enableDecrease, boolean isImmediate) {
+        this.specialThreshold = specialThreshold;
+        this.enableDecrease = enableDecrease;
+        this.isImmediate = isImmediate;
+    }
+
+    public int getSpecialThreshold() {
+        return specialThreshold;
+    }
+
+    public int getEnableDecrease() {
+        return enableDecrease;
+    }
+
+    public boolean isImmediate() {
+        return isImmediate;
+    }
 }
--- app/src/main/java/com/example/androidslimesoccer/Utils.java
+++ app/src/main/java/com/example/androidslimesoccer/Utils.java
@@ -3,14 +3,16 @@ package com.example.androidslimesoccer;
 import android.content.res.Resources;
 
 public class Utils {
-    static int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
-    static int screenHeight =  Resources.getSystem().getDisplayMetrics().heightPixels;
-    static int gravityAcceleration = -10;
-    static int floorFriction = -20;
-    static double assetsScale = 0.5;
-
-    static int slimeStartX = screenWidth / 10;
-    static int slimeStartY = screenHeight / 10;
-    static int slimeMaxSpecialTime = 100;
+    static final int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
+    static final int screenHeight =  Resources.getSystem().getDisplayMetrics().heightPixels;
+    static final int gravityAcceleration = -10;
+    static final int floorFriction = -20;
+    static final double assetsScale = 0.5;
+    static final int slimeStartX = screenWidth / 10;
+    static final int slimeStartY = screenHeight / 10;
+    static final int slimeMaxSpecialTime = 100;
+    static final int slowInitialIncrease = 5;
+    static final int fastInitialIncrease = 20;
+    static final int initialSpecialLevel = 200;
 
 }
