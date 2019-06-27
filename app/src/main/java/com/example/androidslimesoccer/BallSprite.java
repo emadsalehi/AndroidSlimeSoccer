36cc68353cc4fe5f3ea01922563c2b921c3c1666
SinaBolouki <sina.bolouki@gmail.com>
2019-06-27 11:40:00 +0430

    ball sprite and mainThread first touches done

--- app/src/main/java/com/example/androidslimesoccer/BallSprite.java
+++ app/src/main/java/com/example/androidslimesoccer/BallSprite.java
@@ -2,6 +2,20 @@ package com.example.androidslimesoccer;
 
 //TODO Will Be Completed By "SINA"
 
-public class BallSprite {
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 
+public class BallSprite {
+    private Bitmap ballImage;
+
+    public BallSprite(Bitmap ballImage) {
+        this.ballImage = ballImage;
+    }
+
+    public void ballDraw (Canvas canvas) {
+        //TODO left top numbers
+        canvas.drawBitmap(ballImage,100, 100, null);
+    }
+
+    
 }
