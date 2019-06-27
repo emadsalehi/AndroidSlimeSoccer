188b7990e1974d33bb6c8a7b7e86b372c26c8eeb
Emad Salehi <emad.s1178@yahoo.com>
2019-06-27 17:47:21 +0430

    MenuActivity Changed For Running PracticeActivity With Classic Slime.

--- app/src/main/java/com/example/androidslimesoccer/MenuActivity.java
+++ app/src/main/java/com/example/androidslimesoccer/MenuActivity.java
@@ -1,16 +1,27 @@
 package com.example.androidslimesoccer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 //TODO Will Be Completed By "ASHKAN"
 
 public class MenuActivity extends Activity {
 
+    Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intent = new Intent(this, PracticeActivity.class);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        intent.putExtra("SLIME_NAME", "classic");
+        startActivity(intent);
     }
 }
 
