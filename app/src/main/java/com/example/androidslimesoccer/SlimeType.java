dee0f18d3ebb6a00ff6f0e5803a2c4591d4b1d5e
Emad Salehi <emad.s1178@yahoo.com>
2019-06-27 04:31:00 +0430

    SpecialColor Added to SlimeType.

--- app/src/main/java/com/example/androidslimesoccer/SlimeType.java
+++ app/src/main/java/com/example/androidslimesoccer/SlimeType.java
@@ -1,19 +1,26 @@
 package com.example.androidslimesoccer;
 
+
+import android.graphics.Color;
+
 public enum SlimeType {
-    CLASSIC(300, 20, true),
-    INDIAN(400, 400, false),
-    ALIEN(600, 500, true),
-    TRAFIC(400, 200, true);
+    CLASSIC(300, 20, true, Color.RED),
+    INDIAN(400, 400, true, Color.rgb(130, 77, 7)),
+    ALIEN(600, 500, true, Color.GREEN),
+    TRAFIC(400, 200, true, Color.GRAY),
+    RUNNER(100, 20, false, Color.rgb(71, 161, 11));
+
 
     private final int specialThreshold;
     private final int enableDecrease;
     private final boolean isImmediate;
+    private final int color;
 
-    SlimeType(int specialThreshold, int enableDecrease, boolean isImmediate) {
+    SlimeType(int specialThreshold, int enableDecrease, boolean isImmediate, int color) {
         this.specialThreshold = specialThreshold;
         this.enableDecrease = enableDecrease;
         this.isImmediate = isImmediate;
+        this.color = color;
     }
 
     public int getSpecialThreshold() {
@@ -26,5 +33,9 @@ public enum SlimeType {
 
     public boolean isImmediate() {
         return isImmediate;
+    }
+
+    public int getColor() {
+        return color;
     }
 }
