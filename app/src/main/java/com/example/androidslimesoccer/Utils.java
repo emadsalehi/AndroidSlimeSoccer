458b83715287f4fe0e6b0d1e3c4bc6a91e23334c
Emad Salehi <emad.s1178@yahoo.com>
2019-06-27 04:30:30 +0430

    Some Utils Added.

--- app/src/main/java/com/example/androidslimesoccer/Utils.java
+++ app/src/main/java/com/example/androidslimesoccer/Utils.java
@@ -10,9 +10,19 @@ public class Utils {
     static final double assetsScale = 0.5;
     static final int slimeStartX = screenWidth / 10;
     static final int slimeStartY = screenHeight / 10;
+    static final int leftGoalLine = screenWidth / 20;
+    static final int rightGoalLine = screenWidth * 19 / 20;
+    static final int initialXVelocity = screenWidth / 100;
+    static final int initialYVelocity = screenWidth / 50;
     static final int slimeMaxSpecialTime = 100;
     static final int slowInitialIncrease = 5;
     static final int fastInitialIncrease = 20;
     static final int initialSpecialLevel = 200;
-
+    static final int leftRightBorderX = (int)(0.3f * screenWidth);
+    static final int rightUpBorderX = (int)(0.5f * screenWidth);
+    static final int leftSpecialButtonX = screenWidth / 10;
+    static final int leftSpecialButtonY = screenWidth / 5;
+    static final int rightSpecialButtonX = screenWidth * 9 / 10;
+    static final int rightSpecialButtonY = screenWidth / 5;
+    static final int specialButtonHalfSide = screenHeight / 10;
 }
