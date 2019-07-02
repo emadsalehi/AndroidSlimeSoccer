1f06540f624f98e20ed1136391a67227b5433f61
SinaBolouki <sina.bolouki@gmail.com>
2019-07-02 10:57:24 +0430

    SinglePlayerActivity first touches done

--- app/src/main/java/com/example/androidslimesoccer/SinglePlayerActivity.java
+++ app/src/main/java/com/example/androidslimesoccer/SinglePlayerActivity.java
@@ -2,13 +2,21 @@ package com.example.androidslimesoccer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 //TODO Will Be Completed By "SINA"
 
 public class SinglePlayerActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-    }
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        String leftSlimeName = getIntent().getStringExtra("LEFT_SLIME_NAME");
+        String rightSlimeName = getIntent().getStringExtra("RIGHT_SLIME_NAME");
+        setContentView(new SinglePlayerGameView(this, leftSlimeName, rightSlimeName));    }
 }
