46922b4889595689e939b6d2aa91f0a5c339de34
Emad Salehi <emad.s1178@yahoo.com>
2019-07-06 11:52:00 +0430

    Physical Parameters Updated.

--- app/src/main/java/com/example/androidslimesoccer/Utils.java
+++ app/src/main/java/com/example/androidslimesoccer/Utils.java
@@ -5,8 +5,6 @@ import android.content.res.Resources;
 public class Utils {
     static final int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
     static final int screenHeight =  Resources.getSystem().getDisplayMetrics().heightPixels;
-    static final int gravityAcceleration = -(screenWidth / 74) * (screenWidth / 74) / 200;
-    static final int floorFriction = gravityAcceleration / 2;
     static double assetsXScale = 1.0;
     static double assetsYScale = 1.0;
     static final int slimeStartX = screenWidth * 6 / 40;
@@ -14,7 +12,9 @@ public class Utils {
     static final int leftGoalLine = screenWidth * 3 / 40;
     static final int rightGoalLine = screenWidth * 37 / 40;
     static final int initialXVelocity = screenWidth / 100;
-    static final int initialYVelocity = screenWidth / 74;
+    static final int initialYVelocity = screenHeight / 35;
+    static final int gravityAcceleration = -initialYVelocity * initialYVelocity / 280;
+    static final int floorFriction = gravityAcceleration / 2;
     static final int slimeMaxSpecialTime = 100;
     static final int slowInitialIncrease = 1;
     static final int fastInitialIncrease = 3;
