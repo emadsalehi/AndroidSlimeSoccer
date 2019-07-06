e986cee2fe5ea90a6f7c0f9b5fd351e46c31121f
ashkanlarni <ash.larni@gmail.com>
2019-07-06 15:21:10 +0430

    menu created, practiceOnClick handled

--- app/src/main/java/com/example/androidslimesoccer/MenuActivity.java
+++ app/src/main/java/com/example/androidslimesoccer/MenuActivity.java
@@ -2,26 +2,35 @@ package com.example.androidslimesoccer;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 
 //TODO Will Be Completed By "ASHKAN"
 
 public class MenuActivity extends Activity {
 
-    Intent intent;
+    Intent practiceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        intent = new Intent(getBaseContext(), PracticeActivity.class);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        intent.putExtra("SLIME_NAME", "classic");
-        startActivity(intent);
+    }
+
+    public void onPracticeClick(View v) {
+        practiceIntent = new Intent(this, PracticeActivity.class);
+        practiceIntent.putExtra("SLIME_NAME", "classic");
+        startActivity(practiceIntent);
+    }
+
+    public Bitmap getResizedBitmap(Bitmap bm, int newWidth, int newHeight) {
+        return Bitmap.createScaledBitmap(bm, newWidth, newHeight, true);
     }
 }
 
