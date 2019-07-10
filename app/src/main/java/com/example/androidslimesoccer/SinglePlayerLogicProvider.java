428f69ad99a1be9c2405495f1ac828f78635a876
SinaBolouki <sina.bolouki@gmail.com>
2019-07-10 22:24:38 +0430

    single player goal net height check fixed

--- app/src/main/java/com/example/androidslimesoccer/SinglePlayerLogicProvider.java
+++ app/src/main/java/com/example/androidslimesoccer/SinglePlayerLogicProvider.java
@@ -39,13 +39,13 @@ public class SinglePlayerLogicProvider {
 
     public void goalChecker() {
         if (ballSprite.x <= Utils.leftGoalLine &&
-                (Utils.netUpperWallHeight < ballSprite.y + 2 * Utils.ballRatio)) {
+                (Utils.netUpperWallHeight < ballSprite.y )) {
             slime2Goals++;
             slimeSprite1.initializeFirstState();
             slimeSprite2.initializeFirstState();
             ballSprite.initializeFirstState();
         } else if ((ballSprite.x + 2 * Utils.ballRatio) >= Utils.rightGoalLine &&
-                (Utils.netUpperWallHeight < ballSprite.y + 2 * Utils.ballRatio)) {
+                (Utils.netUpperWallHeight < ballSprite.y )) {
             slime1Goals++;
             slimeSprite1.initializeFirstState();
             slimeSprite2.initializeFirstState();
