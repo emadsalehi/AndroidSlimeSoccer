f465bb5b9b095a323caa5c890245ceefdcab349d
SinaBolouki <sina.bolouki@gmail.com>
2019-07-12 12:36:21 +0430

    PracticeView backButton fixed

--- app/src/main/java/com/example/androidslimesoccer/PracticeActivity.java
+++ app/src/main/java/com/example/androidslimesoccer/PracticeActivity.java
@@ -1,6 +1,7 @@
 package com.example.androidslimesoccer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
@@ -17,5 +18,12 @@ public class PracticeActivity extends Activity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         String slimeName = getIntent().getStringExtra("SLIME_NAME");
         setContentView(new PracticeGameView(this, slimeName));
+    }
+
+    @Override
+    public void onBackPressed(){
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+        startActivity(intent);
     }
 }
