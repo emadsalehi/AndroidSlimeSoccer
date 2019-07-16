5bb90639ceeff0b6ef23f76135ab9a76572caa9d
Emad Salehi <emad.s1178@yahoo.com>
2019-07-16 22:20:07 +0430

    SeverReader Modified For TCP Connection.

--- app/src/main/java/com/example/androidslimesoccer/ServerReader.java
+++ app/src/main/java/com/example/androidslimesoccer/ServerReader.java
@@ -1,57 +1,52 @@
 package com.example.androidslimesoccer;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.SocketException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class ServerReader extends Thread {
 
     SlimeSprite slimeSprite;
-    DatagramSocket serverSocket;
+    Socket socket;
+    InputStream inputStream;
     boolean isRunning = true;
 
-    public ServerReader(SlimeSprite slimeSprite, DatagramSocket serverSocket) {
+    public ServerReader(SlimeSprite slimeSprite, Socket socket) {
         this.slimeSprite = slimeSprite;
-        this.serverSocket = serverSocket;
+        this.socket = socket;
     }
 
     @Override
     public void run() {
+        Scanner scanner = null;
         try {
-            byte [] receiveData = new byte[8];
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-
-            while (isRunning) {
-                serverSocket.receive(receivePacket);
-                String[] data = new String(receivePacket.getData()).split(",");
-                Utils.IPAddress = receivePacket.getAddress();
-                if (data[0].equals("c")) {
-                    if (data[1].equals("j")) {
-                        if (slimeSprite.y == Utils.slimeStartY - slimeSprite.slimeImage.getHeight()
-                                && slimeSprite.yVelocity == 0) {
-                            slimeSprite.yVelocity = -Utils.initialYVelocity;
-                        }
-                    } else if (data[1].equals("s")) {
-                        slimeSprite.enableSpecial();
-                    } else {
-                        String metaData = data[1];
-                        boolean value = data[2].equals("t") ? true : false;
-                        if (metaData.equals("r"))
-                            slimeSprite.isMoveRight = value;
-                        else if (metaData.equals("l"))
-                            slimeSprite.isMoveLeft = value;
-                        else
-                            slimeSprite.specialButtonIsHold = value;
-                    }
-                }
-            }
-        } catch (SocketException e) {
-            e.printStackTrace();
+            scanner = new Scanner(socket.getInputStream());
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        while (isRunning) {
+            String[] data = scanner.nextLine().split(",");
+            if (data[0].equals("c")) {
+                if (data[1].equals("j")) {
+                    if (slimeSprite.y == Utils.slimeStartY - slimeSprite.slimeImage.getHeight()
+                            && slimeSprite.yVelocity == 0) {
+                        slimeSprite.yVelocity = -Utils.initialYVelocity;
+                    }
+                } else if (data[1].equals("s")) {
+                    slimeSprite.enableSpecial();
+                } else {
+                    String metaData = data[1];
+                    boolean value = data[2].equals("t") ? true : false;
+                    if (metaData.equals("r"))
+                        slimeSprite.isMoveRight = value;
+                    else if (metaData.equals("l"))
+                        slimeSprite.isMoveLeft = value;
+                    else
+                        slimeSprite.specialButtonIsHold = value;
+                }
+            }
+        }
     }
 
     public void setRunning(boolean running) {
