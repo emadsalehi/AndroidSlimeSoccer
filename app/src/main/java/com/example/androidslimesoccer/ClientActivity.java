6e04e43a3584da8ddab378bf6dfca28d8b8be525
Emad Salehi <emad.s1178@yahoo.com>
2019-07-16 22:23:22 +0430

    ClientActivity Wait For Connection Added.

--- app/src/main/java/com/example/androidslimesoccer/ClientActivity.java
+++ app/src/main/java/com/example/androidslimesoccer/ClientActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import java.net.Socket;
 
 public class ClientActivity extends Activity {
 
@@ -17,8 +18,22 @@ public class ClientActivity extends Activity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         String leftSlimeName = getIntent().getStringExtra("LEFT_SLIME_NAME");
         String rightSlimeName = getIntent().getStringExtra("RIGHT_SLIME_NAME");
+        String ip = getIntent().getStringExtra("IP");
+        System.out.println(ip);
+        ClientConnectorThread clientConnectorThread = new ClientConnectorThread(ip);
+        clientConnectorThread.start();
+        try {
+            synchronized (clientConnectorThread) {
+                if (!clientConnectorThread.isConnected()) {
+                    clientConnectorThread.wait();
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Socket socket = clientConnectorThread.getSocket();
         int goalLimit = getIntent().getIntExtra("GOAL_LIMIT", 5);
-        clientGameView = new ClientGameView(this, leftSlimeName, rightSlimeName);
+        clientGameView = new ClientGameView(this, leftSlimeName, rightSlimeName, socket);
         setContentView(clientGameView);
     }
 }
