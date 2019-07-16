fd93ee9c28ecb0612418119b4784fc9abece4dfc
Emad Salehi <emad.s1178@yahoo.com>
2019-07-16 22:22:56 +0430

    ServerActivity Wait For Connection Added.

--- app/src/main/java/com/example/androidslimesoccer/ServerActivity.java
+++ app/src/main/java/com/example/androidslimesoccer/ServerActivity.java
@@ -2,9 +2,10 @@ package com.example.androidslimesoccer;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import java.net.Socket;
+
 
 public class ServerActivity extends Activity {
 
@@ -19,7 +20,20 @@ public class ServerActivity extends Activity {
         String leftSlimeName = getIntent().getStringExtra("LEFT_SLIME_NAME");
         String rightSlimeName = getIntent().getStringExtra("RIGHT_SLIME_NAME");
         int goalLimit = getIntent().getIntExtra("GOAL_LIMIT", 5);
-        serverGameView = new ServerGameView(this, leftSlimeName, rightSlimeName);
+        System.out.println(getIntent().getStringExtra("IP"));
+        ServerConnectorThread serverConnectorThread = new ServerConnectorThread();
+        serverConnectorThread.start();
+        synchronized (serverConnectorThread) {
+            try {
+                if (!serverConnectorThread.isConnected()) {
+                    serverConnectorThread.wait();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        Socket socket = serverConnectorThread.getSocket();
+        serverGameView = new ServerGameView(this, leftSlimeName, rightSlimeName, socket);
         setContentView(serverGameView);
     }
 }
