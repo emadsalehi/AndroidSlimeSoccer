e7998ae9b8372231c3a14a6bde033ecb249e499b
Emad Salehi <emad.s1178@yahoo.com>
2019-07-20 04:51:40 +0430

    ServerActivity Modified.

--- app/src/main/java/com/example/androidslimesoccer/ServerActivity.java
+++ app/src/main/java/com/example/androidslimesoccer/ServerActivity.java
@@ -1,15 +1,20 @@
 package com.example.androidslimesoccer;
 
 import android.app.Activity;
+import android.net.wifi.p2p.WifiP2pInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 
 public class ServerActivity extends Activity {
 
     ServerGameView serverGameView;
+    WifiP2pInfo info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,20 +25,29 @@ public class ServerActivity extends Activity {
         String leftSlimeName = getIntent().getStringExtra("LEFT_SLIME_NAME");
         String rightSlimeName = getIntent().getStringExtra("RIGHT_SLIME_NAME");
         int goalLimit = getIntent().getIntExtra("GOAL_LIMIT", 5);
-        System.out.println(getIntent().getStringExtra("IP"));
-        ServerConnectorThread serverConnectorThread = new ServerConnectorThread();
-        serverConnectorThread.start();
-        synchronized (serverConnectorThread) {
-            try {
-                if (!serverConnectorThread.isConnected()) {
-                    serverConnectorThread.wait();
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        Socket socket = serverConnectorThread.getSocket();
-        serverGameView = new ServerGameView(this, leftSlimeName, rightSlimeName, socket);
+        System.out.println("Serveeeeeeeeeeeeeeeeer");
+        info = (WifiP2pInfo) getIntent().getExtras().get("INFO");
+        serverGameView = new ServerGameView(this, leftSlimeName, rightSlimeName);
+        getClientInfo.execute();
         setContentView(serverGameView);
     }
+
+    AsyncTask<Void,Void,Void> getClientInfo = new AsyncTask<Void, Void, Void>() {
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            try{
+                Log.d("Chat","waiting for client");
+                ServerSocket serverSocket = new ServerSocket(Utils.serverPort);
+                Socket clientSocket = serverSocket.accept();
+                Log.d("Chat","Client found");
+                serverGameView.startGame(clientSocket);
+            }catch(Exception e){
+                Log.d("Chat","Exception in getClientInfo" + e.getMessage());
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+    };
 }
